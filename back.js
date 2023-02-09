b7a21a7e898672dbbddfc4ebfbe9be6132f0effc
Steven Ho <105683557+StevenH01@users.noreply.github.com>
2023-02-08 16:43:49 -0800

    Create back.js
    working out canvas element to make a pie chart that shows experience(time in years/months)

---
+++ back.js
@@ -0,0 +1,15 @@
+let Donut = document.getElementById("Doughnut")
+
+var data = {
+    lables: [
+        'Java', 'HTML', 'CSS', 'Javascript', 'Python', 'C'
+    ],
+    datasets: [{
+        lables: 'Coding Knowledge (months)',
+        data: [24, 6, 6, 6, 3, 4],
+        backgroundColor: [
+            '#ccaa77', '#eeaa00', '#10aded', '#fbff33', '#14ff14', '#335aff',
+        ],
+        hoverOffset: 4
+    }]
+};
